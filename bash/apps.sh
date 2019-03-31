27b092b157fa0f96101f5d96ee45650d19a82aef
peterzhang <zhh520@gmail.com>
2019-03-31 19:58:41 +0200

    [ADD] settings for t460s

--- bash/apps.sh
+++ bash/apps.sh
@@ -5,19 +5,24 @@ source /usr/local/app/bash-git-prompt/gitprompt.sh
 # Autojump
 . /usr/share/autojump/autojump.bash
 
-# Ruby
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-export GEM_HOME="$HOME/.gem"
-export GEM_PATH="$HOME/.gem"
-export PATH="$GEM_PATH/bin:$PATH"
+## Ruby
+#export PATH="$HOME/.rbenv/bin:$PATH"
+#eval "$(rbenv init -)"
+#export GEM_HOME="$HOME/.gem"
+#export GEM_PATH="$HOME/.gem"
+#export PATH="$GEM_PATH/bin:$PATH"
+#
+## Python
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH="$PYENV_ROOT/bin:$PATH"
+#if command -v pyenv 1>/dev/null 2>&1; then
+#  eval "$(pyenv init -)"
+#fi
 
-# Python
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
+# Go
+export GOROOT="/usr/local/app/go"
+export GOPATH="/u/dev/gocode"
+export PATH="$PATH:$GOROOT/bin"
 
 # Thefuck
 eval $(thefuck --alias)
